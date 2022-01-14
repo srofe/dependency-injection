232c16318df688c83fd71c03afbe15c060fa5416
Simon Rofe <simon.rofe@ieee.org>
2022-01-14 13:05:33 +1000

    Use dependency injection to test Printing

---
+++ di_test.go
@@ -0,0 +1,16 @@
+package dependency_injection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Simon")
+	got := buffer.String()
+	want := "Hello, Simon"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srofe/learn-go-with-tests/dependency-injection
+
+go 1.17
